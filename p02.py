04ce1db8617a0d4692582ef52669efa3774d618a
Derek <sjukledighet@gmail.com>
2019-11-29 18:43:24 -0500

    Add solution for day 2

---
+++ p02.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+
+from aocd import data
+
+
+rows = data.splitlines()
+checksum = 0
+divisible_sum = 0
+
+for row in rows:
+    numbers = [int(n) for n in row.split()]
+    checksum += (max(numbers) - min(numbers))
+    for combo in combinations(numbers, 2):
+        if max(combo) % min(combo) == 0:
+            divisible_sum += max(combo) // min(combo)
+            break
+
+
+print('Part 1:', checksum)
+print('Part 2:', divisible_sum)
